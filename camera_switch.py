c06d59f20407355c35e23b96a6d861f75fa2f139
adnan4290 <shahzadadnan429@gmail.com>
2023-03-27 18:10:37 +0500

    tried to fix a bug

--- camera_switch.py
+++ camera_switch.py
@@ -1,4 +1,4 @@
-import os
+import RPi.GPIO as GPIO
 import time
 
 def switch_camera():
@@ -6,30 +6,35 @@ def switch_camera():
     gpio_17 = 17
     gpio_4 = 4
     
+    # Set GPIO mode to BCM
+    GPIO.setmode(GPIO.BCM)
+
+    # Set GPIO pin modes
+    GPIO.setup(gpio_17, GPIO.OUT)
+    GPIO.setup(gpio_4, GPIO.OUT)
+    
     # Check which camera is currently selected
     camera_a_selected = False
     camera_b_selected = False
-    if os.system("raspi-gpio get {}".format(gpio_17)) == 0:
+    if GPIO.input(gpio_17) == 1:
         camera_a_selected = True
-    elif os.system("raspi-gpio get {}".format(gpio_4)) == 0:
+    elif GPIO.input(gpio_4) == 1:
         camera_b_selected = True
     
     # Switch to the other camera
     if camera_a_selected:
         print("Switching to Camera B")
-        os.system("raspi-gpio set {} op".format(gpio_17))
-        os.system("raspi-gpio set {} dl".format(gpio_17))
-        os.system("raspi-gpio set {} dh".format(gpio_4))
+        GPIO.output(gpio_17, GPIO.LOW)
+        GPIO.output(gpio_4, GPIO.HIGH)
         time.sleep(1)  # Allow time for the camera to release all locks
         os.system("i2cset -y 1 0x70 0x00 0x02")
-        print("camea b selected")
+        print("Camera B selected")
     elif camera_b_selected:
         print("Switching to Camera A")
-        os.system("raspi-gpio set {} op".format(gpio_4))
-        os.system("raspi-gpio set {} dl".format(gpio_4))
-        os.system("raspi-gpio set {} dh".format(gpio_17))
+        GPIO.output(gpio_4, GPIO.LOW)
+        GPIO.output(gpio_17, GPIO.HIGH)
         time.sleep(1)  # Allow time for the camera to release all locks
         os.system("i2cset -y 1 0x70 0x00 0x01")
-        print("camera a selected")
+        print("Camera A selected")
     else:
         print("No camera selected")
